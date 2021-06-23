12703f2c0a271afb15630ef23ecc9e5a5003837f
heyKim <rhandhfl3366@naver.com>
2021-06-24 00:40:34 +0900

    init

---
+++ src/main/java/kiki/controller/TestController.java
@@ -0,0 +1,23 @@
+package kiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String home() {
+		return "This is home";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/homeTest", method=RequestMethod.GET)
+	public String homeTest() {
+		String value = "Test";
+		return value;
+	}
+	
+}
