54442b5c5be18b9f537e749395e5a1bf1a772513
heyKim <rhandhfl3366@naver.com>
2021-06-25 22:35:18 +0900

    To test jenkins webhook

--- src/main/java/kiki/controller/TestController.java
+++ src/main/java/kiki/controller/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
 	@ResponseBody
 	@RequestMapping(value="/homeTest", method=RequestMethod.GET)
 	public String homeTest() {
-		String value = "Test_prd!!";
+		String value = "Test webhook test!!";
 		return value;
 	}
 	
