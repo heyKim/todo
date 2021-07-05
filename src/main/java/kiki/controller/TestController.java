5da647b46a7dba9f2a9168552848dee631a705b5
heyKim <rhandhfl3366@naver.com>
2021-07-05 21:14:57 +0900

    Add call

--- src/main/java/kiki/controller/TestController.java
+++ src/main/java/kiki/controller/TestController.java
@@ -1,12 +1,18 @@
 package kiki.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class TestController {
+	
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String home() {
@@ -19,5 +25,17 @@ public class TestController {
 		String value = "Test webhook test again...!!";
 		return value;
 	}
+	
+	@GetMapping("/liveness")
+	public String liveness() {
+		log.info("liveness check");
+		return "OK";
+	}
+	@GetMapping("/readiness")
+	public String readyness() {
+		log.info("readiness check");
+		return "OK";
+	}
+
 	
 }
